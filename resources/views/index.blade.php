c2212d53b83d1a804b8ea77c5b7b12d02b9886d8
Yerman <gerql1441@gmail.com>
2020-04-11 02:52:28 +0000

    Template & Layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,35 +1,10 @@
-<!DOCTYPE HTML>
-<!--
-	Theory by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
--->
-<html>
-<head>
-    <title>Theory by TEMPLATED</title>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" href="assets/css/main.css" />
-</head>
-<body>
+@extends('layoutPage')
 
-<!-- Header -->
-<header id="header">
-    <div class="inner">
-        <a href="index.html" class="logo">Theory</a>
-        <nav id="nav">
-            <a href="index.html">Home</a>
-            <a href="generic.html">Generic</a>
-            <a href="elements.html">Elements</a>
-        </nav>
-        <a href="#navPanel" class="navPanelToggle"><span class="fa fa-bars"></span></a>
-    </div>
-</header>
-
+@section('content')
 <!-- Banner -->
 <section id="banner">
-    <h1>Welcome to Theory</h1>
-    <p>A free responsive HTML5 website template by TEMPLATED.</p>
+    <h1>Calpulalpan Corazón de Perro</h1>
+    <p>No somos Fundación ni Asociación... Apoyamos por voluntad y con el corazón &hearts;</p>
 </section>
 
 <!-- One -->
@@ -142,30 +117,6 @@
         </div>
     </div>
 </section>
+@endsection
 
-<!-- Footer -->
-<footer id="footer">
-    <div class="inner">
-        <div class="flex">
-            <div class="copyright">
-                &copy; Untitled. Design: <a href="https://templated.co">TEMPLATED</a>. Images: <a href="https://unsplash.com">Unsplash</a>.
-            </div>
-            <ul class="icons">
-                <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-                <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-                <li><a href="#" class="icon fa-linkedin"><span class="label">linkedIn</span></a></li>
-                <li><a href="#" class="icon fa-pinterest-p"><span class="label">Pinterest</span></a></li>
-                <li><a href="#" class="icon fa-vimeo"><span class="label">Vimeo</span></a></li>
-            </ul>
-        </div>
-    </div>
-</footer>
 
-<!-- Scripts -->
-<script src="assets/js/jquery.min.js"></script>
-<script src="assets/js/skel.min.js"></script>
-<script src="assets/js/util.js"></script>
-<script src="assets/js/main.js"></script>
-
-</body>
-</html>

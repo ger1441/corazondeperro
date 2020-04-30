ac82e882b45e92e37d85f4b4626bd03877441c2e
Yerman <gerql1441@gmail.com>
2020-04-30 07:02:45 +0000

    Favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Login | {{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" href="favicon.png">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
--- resources/views/partials/admin/head.blade.php
+++ resources/views/partials/admin/head.blade.php
@@ -5,6 +5,7 @@
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <link rel="icon" href="favicon.png">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="/assets/admin/plugins/fontawesome-free/css/all.min.css">
     <!-- Ionicons -->

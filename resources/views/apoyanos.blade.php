f6962d11c323f5c1533d6f73c8b6fe62ac0b785c
Yerman <gerql1441@gmail.com>
2020-04-30 02:49:27 +0000

    Mensaje de ubicación en sección apóyanos

--- resources/views/apoyanos.blade.php
+++ resources/views/apoyanos.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.page')
+
 @section('content')
     <section id="one" class="wrapper">
         <div class="inner">
@@ -24,6 +25,11 @@
                 Hay temporadas en las que el trabajo escasea y difícilmente la gente nos puede apoyar solicitando servicios, sin embargo cualquier esfuerzo por ayudarnos, sea en <span class="font-blue">efectivo o en especie (alimento, material de curación, material de limpieza)</span>  sin importar la cantidad, siempre será bien recibido.<br>
                     ¡Muchas Gracias!</strong></p>
                 @include('partials.form')
+            </div>
+            <h2>Ubicación</h2>
+            <div class="box">
+                <p><strong>Desgraciadamente hemos pasado por circunstancias poco agradables, desde el <span class="font-blue">abandono de mascotas en nuestra puerta</span> hasta <span class="font-blue">envenenamiento y maltrato a nuestros rescataditos</span>, por lo que nos hemos visto en la necesidad de NO hacer tan pública nuestra ubicación.</strong><p>
+                <p><strong>Sin embargo la gente que nos apoya sabe y conoce perfectamente nuestra situación y que más allá de la ubicación de un albergue, es la ubicación de nuestro hogar que decidimos abrir a los animalitos descobijados.</strong></p>
             </div>
         </div>
     </section>

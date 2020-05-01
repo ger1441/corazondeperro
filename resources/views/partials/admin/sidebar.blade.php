e3c307444a9b1fb722acb49239af2f39ae5ad813
Yerman <gerql1441@gmail.com>
2020-04-30 19:00:00 -0500

    Nombre del logo en Admin y meta robots

--- resources/views/partials/admin/sidebar.blade.php
+++ resources/views/partials/admin/sidebar.blade.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-light-primary elevation-4">
     <!-- Brand Logo -->
     <a href="/admin" class="brand-link">
-        <img src="/images/admin/logo_pequeño.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+        <img src="/images/admin/logo_pequeno.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
              style="opacity: .8">
         <span class="brand-text font-weight-light">Corazón de Perro</span>
     </a>
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,6 +2,7 @@
     <title>Calpulalpan Corazón de Perro</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="robots" content="noindex,nofollow" />
     <link rel="icon" href="favicon.png">
     <link rel="stylesheet" href="/assets/css/main.css" />
     <link rel="stylesheet" href="/assets/css/styles.css">

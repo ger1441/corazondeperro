0ad6c3cf512e87d137ad3b2cde196ae194d7d106
Yerman <gerql1441@gmail.com>
2020-05-07 18:34:35 +0000

    Se agregó enlace para donaciones por PayPal

--- resources/views/apoyanos.blade.php
+++ resources/views/apoyanos.blade.php
@@ -24,15 +24,24 @@
             </div>
             <div class="justifyContent">
                 <h2>Otras maneras de apoyar</h2>
-                <p><strong>Como podrás notar, realizamos diferentes actividades para poder ayudar a los animalitos que lo necesiten, pero sin duda alguna tu ayuda es muy importante para nosotros.<br>
-                Hay temporadas en las que el trabajo escasea y difícilmente la gente nos puede apoyar solicitando servicios, sin embargo cualquier esfuerzo por ayudarnos, sea en <span class="font-blue">efectivo o en especie (alimento, material de curación, material de limpieza)</span>  sin importar la cantidad, siempre será bien recibido.<br>
+                <p><strong>Como podrás notar, realizamos diferentes actividades para poder ayudar a los animalitos que lo necesiten, pero sin duda alguna <span class="font-blue">tu ayuda es muy importante</span> para nosotros.<br>
+                        Hay temporadas en las que el trabajo escasea y difícilmente la gente nos puede apoyar solicitando servicios, sin embargo cualquier esfuerzo por ayudarnos, sea en efectivo o en especie (alimento, material de curación, material de limpieza)  sin importar la cantidad, siempre será bien recibido. Si deseas hacer una <span class="font-blue">aportación voluntaria</span> puedes hacer click en el siguiente botón.<br>
                     ¡Muchas Gracias!</strong></p>
+
+                <p class="text-center">
+                    <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=8NMHL64T8AWK8&source=url" target="_blank">
+                        <img src="/images/paypal_donar.gif" alt="PayPal - La forma segura y fácil de donar" title="PayPal - La forma segura y fácil de donar">
+                    </a>
+                </p>
+
+                <h2>Ubicación</h2>
+                <div class="box">
+                    <p><strong>Desgraciadamente hemos pasado por circunstancias poco agradables, desde el <span class="font-blue">abandono de mascotas en nuestra puerta</span> hasta <span class="font-blue">envenenamiento y maltrato a nuestros rescataditos</span>, por lo que nos hemos visto en la necesidad de NO hacer tan pública nuestra ubicación.</strong><p>
+                    <p><strong>Sin embargo la gente que nos apoya sabe y conoce perfectamente nuestra situación y que más allá de la ubicación de un albergue, es la ubicación de nuestro hogar que decidimos abrir a los animalitos descobijados.</strong></p>
+                </div>
+
+                <h2>Contáctanos!</h2>
                 @include('partials.form')
-            </div>
-            <h2>Ubicación</h2>
-            <div class="box">
-                <p><strong>Desgraciadamente hemos pasado por circunstancias poco agradables, desde el <span class="font-blue">abandono de mascotas en nuestra puerta</span> hasta <span class="font-blue">envenenamiento y maltrato a nuestros rescataditos</span>, por lo que nos hemos visto en la necesidad de NO hacer tan pública nuestra ubicación.</strong><p>
-                <p><strong>Sin embargo la gente que nos apoya sabe y conoce perfectamente nuestra situación y que más allá de la ubicación de un albergue, es la ubicación de nuestro hogar que decidimos abrir a los animalitos descobijados.</strong></p>
             </div>
         </div>
     </section>
@@ -40,4 +49,5 @@
 
 @push('scripts')
     <script src="/assets/js/contact.js"></script>
+
 @endpush
